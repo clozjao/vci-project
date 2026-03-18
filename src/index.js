23d5b1661aa259c2c7453b63e3b55d8fe728afea
cirojao <cirojao@himalayasoft.xyz>
2026-03-18 14:30:21 +0800

    Simplify theme configuration

--- src/index.js
+++ src/index.js
@@ -5,19 +5,14 @@ import { ThemeProvider } from 'styled-components';
 import App from './App';
 // import './utils/i18n';
 import store from './reducers';
-import cloudImg from './themes/cloudImg';
 import GlobalStyle from './themes/GlobalStyle';
-import localImg from './themes/localImg';
 import theme from './themes/theme';
-import { WIDGET_MODE } from './utils/env';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { I18nextProvider } from 'react-i18next';
 import i18n from 'i18next';
 
 // Create a client
 const queryClient = new QueryClient();
-
-const themeImg = WIDGET_MODE === 'on' ? cloudImg : localImg;
 
 let root = ReactDOM.createRoot(document.getElementById('sport-root'));
 
@@ -26,12 +21,10 @@ root.render(
     {/* Provide the client to your App */}
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-        <ThemeProvider theme={themeImg}>
-          <GlobalStyle />
-          <I18nextProvider i18n={i18n}>
-            <App />
-          </I18nextProvider>
-        </ThemeProvider>
+        <GlobalStyle />
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
       </ThemeProvider>
     </QueryClientProvider>
   </Provider>
@@ -63,14 +56,12 @@ window.CRIC.init = () => {
       {/* Provide the client to your App */}
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
-          <ThemeProvider theme={themeImg}>
-            <GlobalStyle />
-            <I18nextProvider i18n={i18n}>
-              <GoogleOAuthProvider clientId="547792569780-87skjjeks1jq1tup8jqp9qrvfsk7hcrs.apps.googleusercontent.com">
-                <App />
-              </GoogleOAuthProvider>
-            </I18nextProvider>
-          </ThemeProvider>
+          <GlobalStyle />
+          <I18nextProvider i18n={i18n}>
+            <GoogleOAuthProvider clientId="547792569780-87skjjeks1jq1tup8jqp9qrvfsk7hcrs.apps.googleusercontent.com">
+              <App />
+            </GoogleOAuthProvider>
+          </I18nextProvider>
         </ThemeProvider>
       </QueryClientProvider>
     </Provider>
